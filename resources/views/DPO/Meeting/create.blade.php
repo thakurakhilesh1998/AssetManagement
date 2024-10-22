8bdf0fce5b122ae13d633bfc38f541e8003705bc
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2024-10-22 21:45:13 +0530

    table created and migration is also added

--- resources/views/DPO/Meeting/create.blade.php
+++ resources/views/DPO/Meeting/create.blade.php
@@ -27,12 +27,27 @@
             </div>
             {{-- Whether Meeting held --}}
             <div class="mb-3">
-                <label for="Month of Meeting" class="form-label">Select Month of Meeting<span class="text-danger">*</span></label>
-                <select name="month" id="monthr" class="form-control">
-                  <option value="-1">--Select Month of Meeting--</option>
-                  <option value="September">September</option>
-                  <option value="October">October</option>
+                <label for="Meeting convened" class="form-label">Whether meeting convened for this month?<span class="text-danger">*</span></label>
+                <select name="meeting_convened" id="meeting_convened" class="form-control">
+                  <option value="-1">--Select if meeting is convened--</option>
+                  <option value="September">Yes</option>
+                  <option value="October">No</option>
                 </select>
+            </div>
+            <div class="mb-3">
+                <label for="Date of Meeting" class="form-label">Date of Meeting<span class="text-danger">*</span></label>
+                <input type="date" name="date" id="date" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="Subject of meeting" class="form-label">Subject of Meeting<span class="text-danger">*</span></label>
+                <input type="text" name="subject" id="subject" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="proceedings">Proceedings of the meeting<span class="text-danger">*</span></label>
+                <input type="file" accept="pdf" name="proceedings" class="form-control">
+            </div>
+            <div class="mb-3">
+                <input type="submit" value="Add Meeting" name="add" id="add" class="form-control btn btn-primary">
             </div>
        </form>
     </div>
